fccac74217a7b8187ed51d7a73109ab8add92373
Kouki Saito <dan.addr.skd@gmail.com>
2020-02-04 13:01:07 +0900

    Bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Folio bot",
-    version="0.5.0",
+    version="0.6.0",
     packages=find_packages(),
     test_suite="test",
     install_requires=[
